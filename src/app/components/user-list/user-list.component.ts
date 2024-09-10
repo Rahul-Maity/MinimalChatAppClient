c12449c6e4648d33d85e033b7f6c43d95fc8cfce
Rahul <mrahulmaity623@gmail.com>
2024-09-10 18:14:30 +0530

    If applied, this commit will update AuthService to fetch and display the current username from the token in the navbar

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit,OnDestroy{
+
+
+  fullname: string | null = null;
+
+  private userPayloadSubscription: Subscription | undefined;
+ 
+  constructor(private auth:AuthService) { }
+  
+ngOnInit(): void {
+  this.userPayloadSubscription = this.auth.userPayload$.subscribe(payload => {
+    this.fullname = payload?.unique_name || null;
+  });
+  }
+  
+  ngOnDestroy(): void {
+    if (this.userPayloadSubscription) {
+      this.userPayloadSubscription.unsubscribe();
+    }
+  }
+
+
+onLogout() {
+  this.auth.signOut();
+}
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,41 +1,74 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Register } from '../models/register.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Login } from '../models/login.model';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private baseUrl: string = "http://localhost:5039/api/";
-  constructor(private http: HttpClient) { }
-  
-  register(user: Register): Observable<any> {
-    const url = `${this.baseUrl}register`; 
-    return this.http.post(url, user); 
-  }
 
-  login(user:Login): Observable<any> {
-    const url = `${this.baseUrl}login`;
+  private userPayload: any;
+  private jwtHelper = new JwtHelperService();
+  private userPayloadSubject = new BehaviorSubject<any>(null);
+  public userPayload$ = this.userPayloadSubject.asObservable();
+  
+
+  constructor(private http: HttpClient,private router:Router) {
+    this.loadUserPayload();
+  }
+  loadUserPayload() {
+    const token = this.getToken();
+    if (token) {
+      this.userPayloadSubject.next(this.jwtHelper.decodeToken(token));
+    } else {
+      this.userPayloadSubject.next(null);
+    }
+  }
+  
+
+  register(user: Register): Observable<any> {
+    const url = `${this.baseUrl}register`;
     return this.http.post(url, user);
   }
-  
 
-  storeToken(tokenValue: string):void
-  {
+  login(user: Login): Observable<any> {
+    const url = `${this.baseUrl}login`;
+   
+
+    return this.http.post(url, user).pipe(
+      tap((res: any) => {
+        this.storeToken(res.token);
+      })
+    );
+  }
+
+
+  storeToken(tokenValue: string): void {
     localStorage.setItem('authToken', tokenValue);
+    this.loadUserPayload();
 
   }
 
-  getToken():string|null
-  {
-    return  localStorage.getItem('authToken');
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
 
   }
 
-  isLoggedIn(): boolean{
+  isLoggedIn(): boolean {
     return !!localStorage.getItem('authToken');
   }
-  
+  signOut() {
+    localStorage.clear();
+    this.userPayloadSubject.next(null);
+    this.router.navigate(['/login']);
+}
+getFullnameFromToken(): string | null {
+  return this.userPayloadSubject.value?.unique_name || null;
+}
+
 }
